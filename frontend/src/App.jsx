98d0a27bfbbe9fc21193106456829636c0aed4ba
Anushka Singh <anushka.singh020307@gmail.com>
2026-01-11 16:26:43 +0530

    Campus and Infrastructure page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Home from './Pages/Home'
 import { Routes,Route} from 'react-router-dom'
 import Branches from './Pages/Branches'
+import Campus from './Pages/Campus'
 
 const App = () => {
   return (
      <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/branches" element={<Branches />} />
+      <Route path="/campus-infra" element={<Campus />} />
     </Routes>
   )
 }
