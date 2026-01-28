6c7aef76683046349ac9c3998c69dc1d9cdbf9ce
Prabhav <prabhav589@gmail.com>
2026-01-28 21:13:25 +0530

    updated cors 3

--- backend/index.js
+++ backend/index.js
@@ -13,11 +13,18 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://experience-iitd-frontend.vercel.app/"],
+    origin: [
+      "http://localhost:5173",
+      "https://experience-iitd-frontend.vercel.app",
+    ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+app.options("*", cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
