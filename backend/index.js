cc2dbbae7d88623520ed5916b72b1491a2d0e87b
Prabhav <prabhav589@gmail.com>
2026-01-29 12:37:54 +0530

    connected local frontend with hosted backend

--- backend/index.js
+++ backend/index.js
@@ -11,18 +11,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
-
-// app.options(/.*/, cors());
-
+app.use(cors({
+  origin: ["http://localhost:5173", "https://experience-iitd-frontend.vercel.app"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -32,5 +26,5 @@ app.use('/api', questionRoutes);
 app.use('/api', AuthMiddleware, protectedRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
